6f4a24eadc5e01d341be748e676184b2545c756d
Actulus <93488786+Actulus@users.noreply.github.com>
2026-02-24 16:13:47 +0200

    lab1_C II

--- 01.labor/lab1_C.hs
+++ 01.labor/lab1_C.hs
@@ -121,19 +121,75 @@ fakt3 res n
   | otherwise = fakt3 (n * res) (n - 1)
 
 -- - az x szám n-ik hatványát, ha a kitevő pozitív szám (3 módszer).
+hatvanyXN x n
+  | n < 0 = error "neg kitevo"
+  | otherwise = x ** n
+
+hatvanyXN2 x n
+  | n < 0 = error "neg kitevo"
+  | otherwise = x ^ n
+
+hatvanyXN3 x n
+  | n < 0 = error "neg kitevo"
+  | n == 0 = 1
+  | otherwise = x * hatvanyXN3 x (n - 1)
 
 -- II. Könyvtárfüggvények használata nélkül, illetve halmazkifejezéseket alkalmazva, definiáljuk azt a függvényt, amely meghatározza:
 
 -- - az első n természetes szám negyzetgyökét,
+negyzetgyok n = [sqrt i | i <- [1 .. n]]
+
 -- - az első n négyzetszámot,
+negyzet n = [i ^ 2 | i <- [1 .. n]]
+
 -- - az első n természetes szám köbét,
+kob n = [i ^ 3 | i <- [1 .. n]]
+
 -- - az első n olyan természetes számot, amelyben nem szerepelnek a négyzetszámok,
+nemNegyzet n = [i | i <- [0 .. n], (sqrt i ^ 2) /= i]
+
 -- - x hatványait adott n-ig,
+hatvany x n = [x ^ i | i <- [0 .. n]]
+
 -- - egy szám páros osztóinak listáját,
+parosOsztok x = [i | i <- [1 .. x], mod x i == 0, mod i 2 == 0]
+
 -- - n-ig a prímszámok listáját,
+osztok x = [i | i <- [1 .. x], mod x i == 0]
+
+primszam x = osztok x == [1, x]
+
+primszamokN n = [i | i <- [2 .. n], primszam i]
+
+primszamokN2 n = [i | i <- [2 .. n], primszamL i]
+  where
+    primszamL ns = osztokL ns == [1, ns]
+    osztokL ns2 = [i | i <- [1 .. ns2], mod ns2 i == 0]
+
 -- - n-ig az összetett számok listáját,
+osszetett n = [i | i <- [1 .. n], primszam i == False]
+
 -- - n-ig a páratlan összetett számok listáját,
+paratlanOsszetett n = [i | i <- [1 .. n], not (primszam i), mod i 2 /= 0]
+
+paratlanOsszetett2 n = [i | i <- [1, 3 .. n], not (primszam i)]
+
 -- - az n-nél kisebb Pitágorászi számhármasokat,
+pitagorasz n = [(a, b, c) | c <- [1 .. n], b <- [1 .. c], a <- [1 .. b], a ** 2 + b ** 2 == c ** 2]
+
 -- - a következő listát: $$[(\texttt{a},0), (\texttt{b},1),\ldots, (\texttt{z}, 25)]$$,
+betuSzam = zip ['a' .. 'z'] [0 .. 25]
+
 -- - a következő listát: $$[(0, 5), (1, 4), (2, 3), (3, 2), (4, 1), (5, 0)]$$, majd általánosítsuk a feladatot.
+szamok = zip [0 .. 5] [5, 4 .. 0]
+
+szamok1 n = zip [0 .. n] [n, n - 1 .. 0]
+
+szamok2 n = [(i, n - i) | i <- [0 .. n]]
+
 -- - azt a listát, ami felváltva tartalmaz True és False értékeket.
+tfLs n = take n ls
+  where
+    ls = [True, False] ++ ls
+
+tfLs2 n = [even i | i <- [0 .. n]]
