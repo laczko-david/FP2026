9a1d5b6e13cc4b22ce90cec38d48e0b7fcf546bc
laczko-david <laczko.arpad.david@student.ms.sapientia.ro>
2026-02-26 11:37:29 +0200

    labor 1 done labor 2 started

---
+++ 02.labor/lab2_A.hs
@@ -0,0 +1,8 @@
+szjSzorzat 0 = 1
+
+szjSzorzat n = mod n 10 * szjSzorzat (div n 10)
+
+szjSzorzat3 n res
+    | n < 0 = error "neg. szam"
+    | div n 10 == 0 = res * n
+    | otherwise = szjSzorzat3 (div n 10 ) (res * (mod n 10))
